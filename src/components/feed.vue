0a91447fc79b0205726a6766b74df4dab530fd0c
Murilo <mbalves1@outlook.com>
2023-05-08 18:29:45 -0300

    [feat]: criando estrutura, vue3 vuetify3 para api gratuita de informacoes

---
+++ src/components/feed.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-card width="500" v-for="item in news" :key="item">
+    <v-img :src="item.urlToImage"></v-img>
+    {{item}}
+  </v-card>
+</template>
+<script>
+import { useStore } from 'vuex'
+import { computed, onMounted } from 'vue'
+
+export default {
+  setup() {
+    const store = useStore()
+
+    const news = computed(() => store.state.news)
+
+    onMounted(() => {
+      store.dispatch('getNews')
+      console.log(news);
+    })
+
+    return {
+      news
+    }
+  }
+}
+</script>
--- src/store/index.js
+++ src/store/index.js
@@ -1,13 +1,31 @@
-import { createStore } from 'vuex'
+import { createStore, useStore } from 'vuex'
+import axios from 'axios'
 
 export default createStore({
   state: {
+    news: []
   },
   getters: {
   },
   mutations: {
+    GET_NEWS(state, data) {
+      console.log(data)
+      state.news = data
+    }
   },
   actions: {
+    // 9d4ed95f048e478390e1790821a58f80
+    getNews() {
+      const store = useStore()
+      axios.get('https://newsapi.org/v2/everything?q=apple&from=2023-05-07&to=2023-05-07&sortBy=popularity&apiKey=9d4ed95f048e478390e1790821a58f80')
+        .then(response => {
+          const { data } = response
+          const { articles } = data
+          store.commit('GET_NEWS', articles)
+          return articles
+        })
+        .catch((error) => console.log(error))
+    }
   },
   modules: {
   }
